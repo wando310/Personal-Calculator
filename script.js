091c60af08d86ee481738b7a574a6a46c291c7e5
Lucas Natan de Almeida Melo <lucasnatan.dev@gmail.com>
2022-06-02 16:12:47 -0400

    improved logic with array.includes() function

--- script.js
+++ script.js
@@ -12,39 +12,43 @@ let equationToShow = '';
 //Variáveis para posição e formatação do ponto '.'
 let pointController = true;
 
+// Arrays de números, operadores e resultados (Não inclue o '.', backspace e delete)
+const numbers = ['0','1','2','3','4','5','6','7','8','9'];
+const operators = ['/','*','-','+','%'];
+const results = ['=','Enter'];
+
 //Funções para receber eventos do teclado
 
 //Monitorador do teclado
 
 document.body.addEventListener('keyup', (e)=>{
     //Condicionais para botões pressionados - Cor e funcionamento
-    if((e.key === '1' ) || (e.key === '2') || (e.key === '3') || (e.key === '4') || (e.key === '5') ||
-    (e.key === '6') || (e.key === '7') || (e.key === '8') || (e.key === '9') || (e.key === '0')){
+
+    if(numbers.includes(e.key)){
         let keyElement = document.querySelector(`button[data-key='${e.key}']`);
         colorButton(keyElement);
         clickNumber(e.key);
-    } if((e.key === '/') || (e.key === '*') || (e.key === '-') || (e.key === '+') || (e.key === '%')){
+    }if(operators.includes(e.key)){
         let keyElement = document.querySelector(`button[data-key='${e.key}']`);
         colorButton(keyElement);
         clickOperator(e.key);
-    } if((e.key === '.')){
-        let keyElement = document.querySelector(`button[data-key='${e.key}']`);
-        colorButton(keyElement);
-        clickPoint(e.key);
-    } if((e.key === 'Delete')){
-        let keyElement = document.querySelector(`button[data-key='${e.key}']`);
-        colorButton(keyElement);
-        clickClearAll();
-    } if((e.key === 'Backspace')){
-        let keyElement = document.querySelector(`button[data-key='${e.key}']`);
-        colorButton(keyElement);
-        clickDelete();
-    } if((e.key === '=') || (e.key === 'Enter')){
+    }if(results.includes(e.key)){
         let keyElement = document.querySelector(`button[data-key='=']`);
         colorButton(keyElement); 
         clickResult(); 
-    }
-});
+    }if((e.key === '.')){
+        let keyElement = document.querySelector(`button[data-key='${e.key}']`);
+        colorButton(keyElement);
+        clickPoint(e.key);
+    }if((e.key === 'Backspace')){
+        let keyElement = document.querySelector(`button[data-key='${e.key}']`);
+        colorButton(keyElement);
+        clickDelete();
+    }if((e.key === 'Delete')){
+        let keyElement = document.querySelector(`button[data-key='${e.key}']`);
+        colorButton(keyElement);
+        clickClearAll();
+}});
 
 //Funções gerais
 //Função para recebimento dos números
@@ -59,6 +63,7 @@ function colorButton(keyElement){
 //Função para recebimento de números de 0 a 1
 function clickNumber(n){
     equationToCalc = equationToCalc + n;
+    let pressedNumberButton = true;
     showResults();
 }
 
@@ -75,21 +80,20 @@ function clickPoint(p){
 function clickOperator(o){
     //Condicionais para caso o operador seja deletado
     //As duas equação toShow e Tocalc são síncronas, ou seja, basta ser verificada uma nesta situação
-    if(((equationToCalc[equationToCalc.length-1] !== o) || (equationToCalc[equationToCalc.length-1] === o))&&((equationToCalc[equationToCalc.length-1] !== '0')&&(equationToCalc[equationToCalc.length-1] !== '1')
-    &&(equationToCalc[equationToCalc.length-1] !== '1')&&(equationToCalc[equationToCalc.length-1] !== '2')&&(equationToCalc[equationToCalc.length-1] !== '3')&&(equationToCalc[equationToCalc.length-1] !== '4')
-    &&(equationToCalc[equationToCalc.length-1] !== '5')&&(equationToCalc[equationToCalc.length-1] !== '6')&&(equationToCalc[equationToCalc.length-1] !== '7')&&(equationToCalc[equationToCalc.length-1] !== '8')
-    &&(equationToCalc[equationToCalc.length-1] !== '9'))){
     
+    if((operators.includes(equationToCalc[equationToCalc.length-1])) && (!numbers.includes(equationToCalc[equationToCalc.length-1]))){
         equationToCalc = equationToCalc.substring(0, equationToCalc.length -1);
         equationToCalc = equationToCalc + o;
         pointController = true;
         showResults();
-    } else{
+    }else if((equationToCalc[equationToCalc.length-1]) === '.'){
+        showResults();
+    }
+    else{
         equationToCalc = equationToCalc + o;
         pointController = true;
         showResults();
     }
-
 }
 
 //Função para apagar números
@@ -97,8 +101,7 @@ function clickDelete(){
     //Filtro caso o '.' seja apagado
     if(equationToCalc[equationToCalc.length - 1] === '.'){
         pointController = true;
-    }else if((equationToCalc[equationToCalc.length - 1] === '%') || (equationToCalc[equationToCalc.length - 1] === '/') || (equationToCalc[equationToCalc.length - 1] === '*') || (equationToCalc[equationToCalc.length - 1] === '-') 
-    || (equationToCalc[equationToCalc.length - 1] === '+')){
+    }else if((operators.includes(equationToCalc[equationToCalc.length-1]))){
         pointController = false;
     }
     equationToCalc = equationToCalc.substring(0, equationToCalc.length -1);
@@ -137,4 +140,3 @@ function showResults(){
     generalEquation.innerHTML = equationToShow;
     generalResult.innerHTML = resultToString;
 }
-
